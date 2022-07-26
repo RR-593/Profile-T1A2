35f404fa25e071270e681502183ab7b50035b68a
RR-593 <Raphael@Munoz.to>
2022-07-26 20:42:29 +1000

    Add content to index.html

--- js/age.js
+++ js/age.js
@@ -1,9 +1,9 @@
 let htmlage = document.getElementById("age");
 
 
-setInterval(function() {
+setInterval(() => {
 
   var age = (new Date() - new Date(962582400000))/ (1000 * 60 * 60 * 24 * 365.25);
   htmlage.innerText = age.toString().substring(0, 12);
 
-}, 80);
+}, 30);
