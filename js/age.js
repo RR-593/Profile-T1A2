6d70aa3f2377b60e6c95da4703f44d24a7f68e5d
RR-593 <Raphael@Munoz.to>
2022-07-26 11:46:25 +1000

    Create age.js

---
+++ js/age.js
@@ -0,0 +1,9 @@
+let htmlage = document.getElementById("age");
+
+
+setInterval(function() {
+
+  var age = (new Date() - new Date(962582400000))/ (1000 * 60 * 60 * 24 * 365.25);
+  htmlage.innerText = age.toString().substring(0, 12);
+
+}, 80);
