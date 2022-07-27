be7447c48f26b7c8f154895d0574ffef37345eeb
RR-593 <Raphael@Munoz.to>
2022-07-27 17:32:45 +1000

    Make buttons change class when clicked

--- js/buttons.js
+++ js/buttons.js
@@ -1,11 +1,15 @@
-skillButtons = document.querySelectorAll('.skill-button');
+const skillButtons = document.querySelectorAll('.skill');
 
+
+console.log(skillButtons);
 for (let button of skillButtons) {
   button.addEventListener('click', () => {
+    
     if (button.className.includes('shown')) {
       button.classList.remove('shown');
       return;
     }
+    for (let button of skillButtons) {button.classList.remove('shown');}
     button.classList.add('shown');
   });
 }
