a8d1520cead62e06ad6b1ef208a19415ec64ec41
RR-593 <Raphael@Munoz.to>
2022-07-27 18:34:12 +1000

    Add modals to buttons

--- js/buttons.js
+++ js/buttons.js
@@ -1,15 +1,22 @@
 const skillButtons = document.querySelectorAll('.skill');
+const modals = document.querySelectorAll('.skill-modal');
 
-
-console.log(skillButtons);
 for (let button of skillButtons) {
   button.addEventListener('click', () => {
     
-    if (button.className.includes('shown')) {
-      button.classList.remove('shown');
+    var modal = document.getElementById(button.getAttribute('modal-name'));
+
+    if (button.className.includes('selected')) {
+      button.classList.remove('selected');
+      modal.classList.remove('shown');
       return;
     }
-    for (let button of skillButtons) {button.classList.remove('shown');}
-    button.classList.add('shown');
+
+    for (let button of skillButtons) {button.classList.remove('selected');}
+    for (let modal of modals) {modal.classList.remove('shown');}
+
+    button.classList.add('selected');
+
+    modal.classList.add('shown');
   });
 }
